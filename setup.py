c0a9d76e1c084e698e19c2a2dee901fac54056b7
Yuurichan-N3 <madiamadia370@email.com>
2026-04-19 18:28:10 +0700

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yuurisan-lib',
+    name='yuurisan',
     version='1.0.0',
-    description='No comment',
-    author='Yuurichan-N3',
+    description='Shared Utility Library for Yuuri Bots',
+    long_description_content_type="text/markdown",
+    author='Yuurisandesu',
+    url='https://github.com/Yuurichan-N3/Yuurisan-lib',
     packages=find_packages(),
     install_requires=[
         'colorama',
         'pyfiglet'
     ],
+    python_requires='>=3.10',
 )
